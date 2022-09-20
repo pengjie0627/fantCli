08c8c2e13858321d017dae98ea0e3dd80a6ed663
caikaijie <caikaijie@ucien.com>
2022-09-20 15:38:45 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
         //     // 在 dist/index.html 的输出
         //     filename: 'index.html',
         //     // 当使用 title 选项时，
-        //     // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+        //     // template 中的 title
         //     title: 'Index Page',
         //     // 在这个页面中包含的块，默认情况下会包含
         //     // 提取出来的通用 chunk 和 vendor chunk。
